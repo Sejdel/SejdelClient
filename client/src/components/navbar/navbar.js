2a2f8c5e90ddf73e19843c84c4d97f123b236fe8
Erik Gralén <dat15egr@student.lu.se>
2020-01-12 18:13:40 +0100

    minor change to navbar

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,15 +1,30 @@
 import React, { Component } from "react";
 import './navbar.css';
+import AppBar from '@material/core/AppBar'
+import Toolbar from '@material/core/ToolBar'
+import IconButton from '@material/core/IconButton'
+import MenuIcon from '@material/core/MenuIcon'
+import classes from '@material/core/classes'
+import Typography from '@material/core/Typography'
+import Button from '@material/core/Button'
+
+
+
  
 class NavBar extends Component {
   render() {
     return (
-<ul>
-  <li><a href="">Home</a></li>
-  <li><a href="">Memes</a></li>
-  <li><a href="">High Score</a></li>
-  <li><a href="">About</a></li>
-</ul> 
+        <AppBar position="static">
+        <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+            News
+            </Typography>
+            <Button color="inherit">Login</Button>
+        </Toolbar>
+        </AppBar>
     );
   }
 }
