5544803ececacf42881c37e22a8b822c0f569081
Simon Hyttfors <simonhyttfors@gmail.com>
2020-01-12 19:28:59 +0100

    Update style for StaticPage

--- client/src/components/static_pages/StaticPage.js
+++ client/src/components/static_pages/StaticPage.js
@@ -7,7 +7,7 @@ function StaticPage() {
       <h1 class="lemma">&ldquo; Sejdelsöndag &rdquo;</h1>
       <h3 class="phonetic">|sej`delsön´dag|</h3>
 
-      <p>Sammansättning av <span class="reference">sejdel</span> och <span class="reference">söndag</span>, en </p>
+      <p>Sammansättning av <span class="reference">sejdel</span> och <span class="reference">söndag</span>, en mötesplats för unga studenter och handelsresande. Äger traditionellt sett rum varje söndag klockan 19:00 (UTC+2) hemma hos Gräkan med ett fåtal undantag. Öl och andra former av alkohol inmundigas i mängder.</p>
     </div>
   );
 }
