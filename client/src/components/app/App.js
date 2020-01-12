321ca48b222c39c4720f888483c475ba43176260
Simon Hyttfors <simonhyttfors@gmail.com>
2020-01-12 18:41:19 +0100

    Remove default placeholder text from App.js

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -10,22 +10,6 @@ function App() {
 
       <NavBar />
 
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-
-        </a>
-      </header>
-      SejdelSöndag
-
       <Button variant="contained" color="primary">
         f
       </Button>
