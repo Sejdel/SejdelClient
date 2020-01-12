c00453dd3bb1bb9ca5dd91e3b8795d747ac1a7dc
Simon Hyttfors <simonhyttfors@gmail.com>
2020-01-12 18:03:28 +0100

    Add static page

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import './App.css';
+import StaticPage from '../static_pages/StaticPage'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-         
-        </a>
-      </header>
-      SejdelSöndag
+      <StaticPage />
     </div>
   );
 }
