814a76a8cb9c1d584d8cbd8a9a3b0c948827435b
Simon Hyttfors <simonhyttfors@gmail.com>
2020-01-14 20:25:24 +0100

    Change text in StaticPage to test deploy hook

--- client/src/components/static_pages/StaticPage.js
+++ client/src/components/static_pages/StaticPage.js
@@ -4,13 +4,13 @@ import './StaticPage.css';
 function StaticPage() {
 
   let state = { apiResponse: "a" };
-  
+
   return (
     <div className="content">
       <h1 class="lemma">&ldquo; Sejdelsöndag &rdquo;</h1>
       <h3 class="phonetic">|sej`delsön´dag|</h3>
 
-      <p>Sammansättning av <span class="reference">sejdel</span> och <span class="reference">söndag</span>, en mötesplats för unga studenter och handelsresande. Äger traditionellt sett rum varje söndag klockan 19:00 (GMT+1) hemma hos Gräkan med ett fåtal undantag. Öl och andra former av alkohol inmundigas i mängder.</p>
+      <p>Sammansättning av <span class="reference">sejdel</span> och <span class="reference">söndag</span>, en mötesplats för unga studenter och handelsresande. Äger traditionellt sett rum varje söndag klockan 19:00 (GMT+1) hemma hos Gräkan med ett fåtal undantag. Öl samt andra former av alkohol inmundigas i mängder.</p>
     </div>
   );
 }
